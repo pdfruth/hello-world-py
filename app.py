aa8f6384214ae8e7d35dd98ddb3c8f6bf42620df
pdfruth <pat@patfruth.com>
2024-03-01 08:43:11 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, make_response, g
+from redis import Redis
+import os
+import platform
+import socket
+import random
+import json
+
+#  This is a comment
+
+hostname = socket.gethostname()
+proc = platform.processor()
+
+# may have to have env variable for Z
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    resp = make_response(render_template(
+        'index.html',
+        hostname=hostname,
+        proc=proc,
+    ))
+    return resp
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
